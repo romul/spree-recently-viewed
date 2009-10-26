8de8467c3a0b038e6c5b2ef942e95b4cbce678d2
Roman Smirnov <roman@railsdog.com>
2009-10-26 18:44:16 +0300

    Removing logic from view

--- recently_viewed_extension.rb
+++ recently_viewed_extension.rb
@@ -8,5 +8,25 @@ class RecentlyViewedExtension < Spree::Extension
   
   def activate
     Spree::Config.set(:recently_viewed_products_max_count => 5) if Spree::Config.instance
+    
+    Spree::BaseHelper.class_eval do
+      def get_recently_viewed_products_ids
+        if session['recently_viewed_products'].nil? 
+          []
+        else   
+          session['recently_viewed_products'].split(', ')
+        end
+      end
+    end
+    
+    Product.class_eval do
+      def self.find_by_array_of_ids(ids)
+        ids.map! { |id| id.to_i }
+        products = Product.find(:all, :conditions => ["id IN (?)", ids])
+        sorted_products = Array.new(products.size)
+        products.map { |p| sorted_products[ids.index(p.id)] = p }
+        sorted_products      
+      end
+    end
   end
 end
