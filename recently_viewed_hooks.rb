7ebc40dbb13520eb6b1133d23678dd04b6fb6cfd
David North <davidnorth@railsdog.com>
2009-12-15 21:30:42 +0000

    add stylesheet that calls metal using inside_head theme hook

---
+++ recently_viewed_hooks.rb
@@ -0,0 +1,9 @@
+class RecentlyViewedHooks < Spree::ThemeSupport::HookListener
+  
+  insert_after :inside_head do
+    if @product
+      stylesheet_link_tag("/recently/viewed/products.css?product_id=#{@product.id}")
+    end
+  end
+    
+end
