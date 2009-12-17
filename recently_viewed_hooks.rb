7838c3db21c0f4757eb76cdf8f772094736b3f41
David North <davidnorth@railsdog.com>
2009-12-17 21:31:12 +0000

    update hook to new erb style

--- recently_viewed_hooks.rb
+++ recently_viewed_hooks.rb
@@ -1,9 +1,11 @@
 class RecentlyViewedHooks < Spree::ThemeSupport::HookListener
   
   insert_after :inside_head do
-    if @product
-      stylesheet_link_tag("/recently/viewed/products.css?product_id=#{@product.id}")
-    end
+    '
+    <% if @product %>
+      <%= stylesheet_link_tag("/recently/viewed/products.css?product_id=#{@product.id}") %>
+    <% end %>
+    '
   end
     
 end
